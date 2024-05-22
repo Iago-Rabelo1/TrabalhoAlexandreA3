a1a86e637879a232b4742e1f4208aefec3259a6b
Iago-Rabelo1 <iago_v.r.s@hotmail.com>
2024-05-22 16:38:06 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,31 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'
-
-def calcular_perfil_investidor(respostas):
-    pontuacao = 0
+@app.route('/resultado', methods=['GET'])
+def resultado():
+    perfil = session.get('perfil')
+    sugestoes = session.get('sugestoes')
+    nome = session.get('nome')
+    sobrenome = session.get('sobrenome')
     
-    if respostas['resposta1'] == "a":
-        pontuacao += 1
-    elif respostas['resposta1'] == "b":
-        pontuacao -= 1
+    if perfil is None or sugestoes is None:
+        return redirect(url_for('perguntas'))
 
-    if respostas['resposta2'] == "a":
-        pontuacao -= 1
-    elif respostas['resposta2'] == "b":
-        pontuacao += 1
-    elif respostas['resposta2'] == "c":
-        pontuacao += 2
+    return render_template('resultado.html', perfil=perfil, sugestoes=sugestoes, nome=nome, sobrenome=sobrenome)
 
-    if respostas['resposta3'] == "a":
-        pontuacao -= 1
-    elif respostas['resposta3'] == "b":
-        pontuacao += 1
-    elif respostas['resposta3'] == "c":
-        pontuacao += 2
-
-    if respostas['resposta4'] == "a":
-        pontuacao -= 1
-    elif respostas['resposta4'] == "c":
-        pontuacao += 1
+if __name__ == '__main__':
+    app.run(debug=True)
