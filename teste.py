e5623c0e65ffa627b7f78aa3b4957433a158f114
Iago-Rabelo1 <iago_v.r.s@hotmail.com>
2024-05-22 16:42:12 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -29,3 +29,18 @@ def calcular_perfil_investidor(respostas):
         pontuacao -= 1
     elif respostas['resposta4'] == "c":
         pontuacao += 1
+
+@app.route('/resultado', methods=['GET'])
+def resultado():
+    perfil = session.get('perfil')
+    sugestoes = session.get('sugestoes')
+    nome = session.get('nome')
+    sobrenome = session.get('sobrenome')
+    
+    if perfil is None or sugestoes is None:
+        return redirect(url_for('perguntas'))
+
+    return render_template('resultado.html', perfil=perfil, sugestoes=sugestoes, nome=nome, sobrenome=sobrenome)
+
+if __name__ == '__main__':
+    app.run(debug=True)
